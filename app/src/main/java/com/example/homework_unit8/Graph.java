f36129648a8e271c97010af838e125d939c68f69
Bryan Hennes <Bryanhennes@gmail.com>
2021-11-21 11:56:19 -0500

    started on dymanically changing graph

--- app/src/main/java/com/example/homework_unit8/Graph.java
+++ app/src/main/java/com/example/homework_unit8/Graph.java
@@ -1,11 +1,38 @@
 package com.example.homework_unit8;
 
 
+import static com.example.homework_unit8.MainActivity.AMOUNT1;
+import static com.example.homework_unit8.MainActivity.AMOUNT10;
+import static com.example.homework_unit8.MainActivity.AMOUNT11;
+import static com.example.homework_unit8.MainActivity.AMOUNT2;
+import static com.example.homework_unit8.MainActivity.AMOUNT3;
+import static com.example.homework_unit8.MainActivity.AMOUNT4;
+import static com.example.homework_unit8.MainActivity.AMOUNT5;
+import static com.example.homework_unit8.MainActivity.AMOUNT6;
+import static com.example.homework_unit8.MainActivity.AMOUNT7;
+import static com.example.homework_unit8.MainActivity.AMOUNT8;
+import static com.example.homework_unit8.MainActivity.AMOUNT9;
+import static com.example.homework_unit8.MainActivity.GRADE1;
+import static com.example.homework_unit8.MainActivity.GRADE10;
+import static com.example.homework_unit8.MainActivity.GRADE11;
+import static com.example.homework_unit8.MainActivity.GRADE2;
+import static com.example.homework_unit8.MainActivity.GRADE3;
+import static com.example.homework_unit8.MainActivity.GRADE4;
+import static com.example.homework_unit8.MainActivity.GRADE5;
+import static com.example.homework_unit8.MainActivity.GRADE6;
+import static com.example.homework_unit8.MainActivity.GRADE7;
+import static com.example.homework_unit8.MainActivity.GRADE8;
+import static com.example.homework_unit8.MainActivity.GRADE9;
+import static com.example.homework_unit8.MainActivity.MYPREF;
+import static com.example.homework_unit8.MainActivity.lettersAmount;
+
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.content.Intent;
 
@@ -17,21 +44,47 @@ public class Graph extends View {
     private Paint mPaint2;
     private Paint axisPaint;
     float barWidth;
-    float barHeight = 20f;
+    float barHeight;
     float graphMargin = 110f;
     float origin; //(0,0) on graph
     float graphWidth;
-
-
+    float screenWidth;
+    float screenHeight;
+    float barSpace = 20f;
+    SharedPreferences sharedPreferences;
+    public String[] letterGrades;
+    public int[] letterAmounts;
+    int totalLetters;
+    int sharedPreferencesInt;
+    public static String[] keys = {GRADE1, GRADE2, GRADE3, GRADE4, GRADE5, GRADE6, GRADE7, GRADE8, GRADE9, GRADE10, GRADE11};
+    public static String[] amounts = {AMOUNT1, AMOUNT2, AMOUNT3, AMOUNT4, AMOUNT5, AMOUNT6, AMOUNT7, AMOUNT8, AMOUNT9, AMOUNT10, AMOUNT11};
+    Paint[] colors = new Paint[3];
+    int[] colorResources = {getResources().getColor(R.color.bar1), getResources().getColor(R.color.bar2), getResources().getColor(R.color.bar3)};
 
 
     public Graph(Context context, AttributeSet attrs) {
         super(context, attrs);
 
+        sharedPreferences = context.getSharedPreferences(MYPREF, Context.MODE_PRIVATE);
+        sharedPreferencesInt = sharedPreferences.getInt(lettersAmount, totalLetters);
+        Log.d("total amount of letters: ", ""+sharedPreferencesInt);
+
+        getSharedPreferences();
+        checkSharedPreferences();
+
 
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint2 = new Paint(Paint.ANTI_ALIAS_FLAG);
         axisPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        //set array of paints for bars in graph
+        for(int i =0; i < colors.length; i++){
+            colors[i] = new Paint(Paint.ANTI_ALIAS_FLAG);
+            colors[i].setColor(colorResources[i]);
+        }
+
+
+
     }
 
     @Override
@@ -39,15 +92,16 @@ public class Graph extends View {
         super.onDraw(canvas);
 
 
-        float screenWidth = canvas.getWidth();
-        float screenHeight = canvas.getHeight();
+        screenWidth = canvas.getWidth();
+        screenHeight = canvas.getHeight();
+
         origin = screenHeight- graphMargin;
 
         //width of entire graph space
         graphWidth = graphMargin + (screenWidth - graphMargin);
 
         //try to determine bar width dynamically depending on number of different grades to graph
-        barWidth = graphWidth /6 -20;
+        barWidth = graphWidth /sharedPreferencesInt - 100;
 
 
         mPaint.setColor(Color.RED);
@@ -65,7 +119,42 @@ public class Graph extends View {
             canvas.drawRect(graphMargin-30f, i-10, graphMargin, i, axisPaint);
         }*/
 
-        canvas.drawRect(graphMargin+10, graphMargin,  graphMargin+10 + barWidth, screenHeight-graphMargin, mPaint);
+        //canvas.drawRect(graphMargin+10, graphMargin,  graphMargin+10 + barWidth, screenHeight-graphMargin, mPaint);
+        drawRects(canvas);
+    }
+
+    public void getSharedPreferences(){
+        String letter = "";
+        int amount = 0;
+        letterGrades = new String[sharedPreferencesInt];
+        letterAmounts = new int[sharedPreferencesInt];
+
+        for(int i = 0; i < sharedPreferencesInt; i++){
+            letterGrades[i] = sharedPreferences.getString(keys[i], letter);
+            letterAmounts[i] = sharedPreferences.getInt(amounts[i], amount);
+        }
+        Log.d("arrays:", ""+letterGrades);
+    }
+
+    public void checkSharedPreferences(){
+        for(int i = 0; i < sharedPreferencesInt; i++){
+            Log.d("grades:", ""+letterGrades[i]);
+            Log.d("amounts:", ""+letterAmounts[i]);
+        }
+    }
+
+    //dynamically draw bars depending on amount of letter grades inputted
+    public void drawRects(Canvas canvas){
+        int c = 0;
+        float originalPlacement = graphMargin+10;
+        for(int i =0; i< sharedPreferencesInt; i++, originalPlacement += barWidth, c++){
+            if(i == 3){
+                c= 0;
+            }
+            //dyamically change top of bar placement depending on value
+            canvas.drawRect(originalPlacement, (screenHeight-graphMargin)-letterAmounts[i]*100,  originalPlacement + barWidth, screenHeight-graphMargin, colors[c]);
+        }
+
     }
 
 }
--- app/src/main/java/com/example/homework_unit8/MainActivity.java
+++ app/src/main/java/com/example/homework_unit8/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.homework_unit8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -18,6 +20,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayout ll;
@@ -28,8 +31,39 @@ public class MainActivity extends AppCompatActivity {
     Button computeButton;
     EditText editTextArray[] = new EditText[20];
     EditText numOfStudents;
+    public static final String GRADE1 = "FIRST_GRADE_KEY";
+    public static final String GRADE2 = "SECOND_GRADE_KEY";
+    public static final String GRADE3 = "THIRD_GRADE_KEY";
+    public static final String GRADE4 = "FOURTH_GRADE_KEY";
+    public static final String GRADE5 = "FIFTH_GRADE_KEY";
+    public static final String GRADE6 = "SIXTH_GRADE_KEY";
+    public static final String GRADE7 = "SEVENTH_GRADE_KEY";
+    public static final String GRADE8 = "EIGHTH_GRADE_KEY";
+    public static final String GRADE9 = "NINTH_GRADE_KEY";
+    public static final String GRADE10 = "TENTH_GRADE_KEY";
+    public static final String GRADE11 = "ELEVENTH_GRADE_KEY";
+    public static final String AMOUNT1 = "LETTER_AMOUNT_ONE";
+    public static final String AMOUNT2 = "LETTER_AMOUNT_TWO";
+    public static final String AMOUNT3 = "LETTER_AMOUNT_THREE";
+    public static final String AMOUNT4 = "LETTER_AMOUNT_FOUR";
+    public static final String AMOUNT5 = "LETTER_AMOUNT_FIVE";
+    public static final String AMOUNT6 = "LETTER_AMOUNT_SIX";
+    public static final String AMOUNT7 = "LETTER_AMOUNT_SEVEN";
+    public static final String AMOUNT8 = "LETTER_AMOUNT_EIGHT";
+    public static final String AMOUNT9 = "LETTER_AMOUNT_NINE";
+    public static final String AMOUNT10 = "LETTER_AMOUNT_TEN";
+    public static final String AMOUNT11 = "LETTER_AMOUNT_ELEVEN";
+    public static final String lettersAmount = "TOTAL_DIFFERENT_LETTER_GRADES";
+    //array of sharedpreference keys for letter grade
+    public static String[] keys = {GRADE1, GRADE2, GRADE3, GRADE4, GRADE5, GRADE6, GRADE7, GRADE8, GRADE9, GRADE10, GRADE11};
+    //array of sharedpreference keys for grade amounts
+    public static String[] amounts = {AMOUNT1, AMOUNT2, AMOUNT3, AMOUNT4, AMOUNT5, AMOUNT6, AMOUNT7, AMOUNT8, AMOUNT9, AMOUNT10, AMOUNT11};
+    SharedPreferences sharedPreferences;
+    public static final String MYPREF = "MY_PREF_FILE";
+    public static final String[] acceptableGrades = {"a", "a+", "a-", "b", "b+", "b-", "c", "c+", "c-", "d", "f"};
 
     int totalStudents;
+
     HashMap<String, Integer> gradeAmounts = new HashMap<String, Integer>();
 
     @Override
@@ -43,6 +77,8 @@ public class MainActivity extends AppCompatActivity {
         mainET = new EditText(this);
         getFieldsButton = new Button(this);
         computeButton = new Button(this);
+
+        sharedPreferences = getSharedPreferences(MYPREF, Context.MODE_PRIVATE);
 
 
 
@@ -92,14 +128,14 @@ public class MainActivity extends AppCompatActivity {
         computeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*if(!areFieldsCompleted()) {
+                if(!areFieldsCompleted()) {
                     Toast.makeText(MainActivity.this, "Cannot leave any fields blank", Toast.LENGTH_LONG).show();
                 }
                 else{
                     populateGradeAmounts();
                     startGraphActivity(view);
-                }*/
-                startGraphActivity(view);
+                }
+                //startGraphActivity(view);
 
             }
         });
@@ -162,9 +198,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void populateGradeAmounts(){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
         for(int i = 0; i < inputArray.length; i++){
             gradeAmounts.put(inputArray[i], Integer.parseInt(editTextArray[i].getText().toString()));
+            editor.putString(keys[i], inputArray[i]);
+            editor.putInt(amounts[i], Integer.parseInt(editTextArray[i].getText().toString()));
         }
+        //put the total number of different letters
+        editor.putInt(lettersAmount, inputArray.length);
+        //editor.commit();
+        editor.apply();
         Log.d("GradeAmounts: ", ""+gradeAmounts);
     }
 
@@ -202,6 +245,16 @@ public class MainActivity extends AppCompatActivity {
     public void pushValuesToView(View view){
         Intent intent = new Intent(this, Graph.class);
         intent.putExtra("map", gradeAmounts);
+    }
+
+    public void setSharedPreferences(HashMap<String, Integer> amounts){
+
+        for (Map.Entry<String, Integer> entry : amounts.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+
+
+        }
     }
 
 
